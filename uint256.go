14a632e6a125e1755e8745b363314d9b00e1440e
wuuuk <ronnylin8@gmail.com>
2024-03-19 11:38:40 +0800

    update uint256 type

--- uint256.go
+++ uint256.go
@@ -9,8 +9,8 @@ import (
 
 // Uint256 is like `u256` in move.
 type Uint256 struct {
-	lo uint64
-	hi uint64
+	lo Uint128
+	hi Uint128
 }
 
 var (
@@ -20,25 +20,23 @@ var (
 )
 
 func (u Uint256) Big() *big.Int {
-	loBig := NewBigIntFromUint64(u.lo)
-	hiBig := NewBigIntFromUint64(u.hi)
-	hiBig = hiBig.Lsh(hiBig, 64)
+	loBig := u.lo.Big()
+	hiBig := u.hi.Big()
+	hiBig = hiBig.Lsh(hiBig, 128)
 
 	return hiBig.Add(hiBig, loBig)
 }
 
-func (i Uint256) MarshalJSON() ([]byte, error) {
-	return json.Marshal(i.Big().String())
+func (u Uint256) MarshalJSON() ([]byte, error) {
+	return json.Marshal(u.Big().String())
 }
-
-var maxU256 = (&big.Int{}).Lsh(big.NewInt(1), 256)
 
 func checkUint256(bigI *big.Int) error {
 	if bigI.Sign() < 0 {
 		return fmt.Errorf("%s is negative", bigI.String())
 	}
 
-	if bigI.Cmp(maxU256) >= 0 {
+	if bigI.BitLen() > 256 {
 		return fmt.Errorf("%s is greater than Max Uint 256", bigI.String())
 	}
 
@@ -50,15 +48,21 @@ func (u *Uint256) SetBigInt(bigI *big.Int) error {
 		return err
 	}
 
-	r := make([]byte, 0, 16)
+	r := make([]byte, 0, 32)
 	bs := bigI.Bytes()
-	for i := 0; i+len(bs) < 16; i++ {
+	for i := 0; i+len(bs) < 32; i++ {
 		r = append(r, 0)
 	}
 	r = append(r, bs...)
 
-	hi := binary.BigEndian.Uint64(r[0:8])
-	lo := binary.BigEndian.Uint64(r[8:16])
+	lo := Uint128{
+		lo: binary.BigEndian.Uint64(r[24:32]),
+		hi: binary.BigEndian.Uint64(r[16:24]),
+	}
+	hi := Uint128{
+		lo: binary.BigEndian.Uint64(r[8:16]),
+		hi: binary.BigEndian.Uint64(r[0:8]),
+	}
 
 	u.hi = hi
 	u.lo = lo
@@ -76,7 +80,7 @@ func (u *Uint256) UnmarshalText(data []byte) error {
 	return u.SetBigInt(bigI)
 }
 
-func (i *Uint256) UnmarshalJSON(data []byte) error {
+func (u *Uint256) UnmarshalJSON(data []byte) error {
 	var dataStr string
 	if err := json.Unmarshal(data, &dataStr); err != nil {
 		return err
@@ -88,53 +92,48 @@ func (i *Uint256) UnmarshalJSON(data []byte) error {
 		return fmt.Errorf("failed to parse %s as an integer", dataStr)
 	}
 
-	return i.SetBigInt(bigI)
+	return u.SetBigInt(bigI)
 }
 
 func NewUint256FromBigInt(bigI *big.Int) (*Uint256, error) {
-	i := &Uint256{}
+	u := &Uint256{}
 
-	if err := i.SetBigInt(bigI); err != nil {
+	if err := u.SetBigInt(bigI); err != nil {
 		return nil, err
 	}
 
-	return i, nil
+	return u, nil
 }
 
 func NewUint256(s string) (*Uint256, error) {
-	r := &big.Int{}
-	r, ok := r.SetString(s, 10)
+	b := &big.Int{}
+	b, ok := b.SetString(s, 10)
 	if !ok {
 		return nil, fmt.Errorf("failed to parse %s as an integer", s)
 	}
 
-	return NewUint256FromBigInt(r)
+	return NewUint256FromBigInt(b)
 }
 
-func (i Uint256) MarshalBCS() ([]byte, error) {
-	r := make([]byte, 16)
+func (u Uint256) MarshalBCS() ([]byte, error) {
+	r := make([]byte, 32)
 
-	binary.LittleEndian.PutUint64(r, i.lo)
-	binary.LittleEndian.PutUint64(r[8:], i.hi)
+	binary.LittleEndian.PutUint64(r, u.hi.lo)
+	binary.LittleEndian.PutUint64(r[8:], u.hi.hi)
+	binary.LittleEndian.PutUint64(r[16:], u.lo.lo)
+	binary.LittleEndian.PutUint64(r[32:], u.lo.hi)
 
 	return r, nil
 }
 
 func (i *Uint256) Cmp(j *Uint256) int {
 	switch {
-	case i.hi > j.hi || (i.hi == j.hi && i.lo > j.lo):
+	case i.hi.Cmp(&j.hi) > 0 || (i.hi.Cmp(&j.hi) == 0 && i.lo.Cmp(&j.lo) > 0):
 		return 1
-	case i.hi == j.hi && i.lo == j.lo:
+	case i.hi.Cmp(&j.hi) == 0 && i.lo.Cmp(&j.lo) == 0:
 		return 0
 	default:
 		return -1
-	}
-}
-
-func NewUint256FromUint64(lo, hi uint64) *Uint256 {
-	return &Uint256{
-		lo: lo,
-		hi: hi,
 	}
 }
 
